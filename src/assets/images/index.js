47f711b78f1bebefaa415db96645a0711d08961e
Tarun-Zenitsu <btkkumbhar1998@gmail.com>
2023-11-13 23:33:06 +0530

    final project

---
+++ src/assets/images/index.js
@@ -0,0 +1,25 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import school from './school.png'
+import college from './college.png'
+import degree from './degree.png'
+import kopykitab from './kopykitab.png'
+import webdev from './webdev.jpg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    school,
+    college, 
+    degree, 
+    kopykitab, 
+    webdev
+}
